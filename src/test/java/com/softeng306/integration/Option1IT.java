972c133b6a9f9e51f15a3dcd45008ab7ecc0cf11
Lucas Gao <lucas2005gao@gmail.com>
2020-10-22 11:57:30 +1300

    made Option1IT class for add student

--- src/test/java/com/softeng306/integration/Option1IT.java
+++ src/test/java/com/softeng306/integration/Option1IT.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
-public class MainIT {
+public class Option1IT {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
@@ -54,8 +54,9 @@ public class MainIT {
     }
 
     @Test
-    public void testMain(){
-        inputsBuilder.append("11").append(System.lineSeparator());
+    public void testAddStudent(){
+        inputsBuilder.append("1").append(System.lineSeparator());
+        inputsBuilder.append("1").append(System.lineSeparator());
 
         preparingInputStream();
         Main.main(new String[]{});
@@ -66,70 +67,6 @@ public class MainIT {
         expectedOutput.appendLine("Permission has been granted by the original developers to anonymize the code and use for education purposes.");
         expectedOutput.appendLine("******************************************************************************************************************************");
         expectedOutput.appendLine();
-
-        expectedOutput.appendLine("************ I can help you with these functions: *************");
-        expectedOutput.appendLine(" 0. Print Options");
-        expectedOutput.appendLine(" 1. Add a student");
-        expectedOutput.appendLine(" 2. Add a course");
-        expectedOutput.appendLine(" 3. Register student for a course including tutorial/lab classes");
-        expectedOutput.appendLine(" 4. Check available slots in a class (vacancy in a class)");
-        expectedOutput.appendLine(" 5. Print student list by lecture, tutorial or laboratory session for a course");
-        expectedOutput.appendLine(" 6. Enter course assessment components weightage");
-        expectedOutput.appendLine(" 7. Enter coursework mark – inclusive of its components");
-        expectedOutput.appendLine(" 8. Enter exam mark");
-        expectedOutput.appendLine(" 9. Print course statistics");
-        expectedOutput.appendLine("10. Print student transcript");
-        expectedOutput.appendLine("11. Quit Main System");
-        expectedOutput.appendLine();
-
-        expectedOutput.appendLine("Enter your choice, let me help you:");
-
-        expectedOutput.appendLine("Backing up data before exiting...");
-        expectedOutput.appendLine("********* Bye! Thank you for using Main! *********");
-        expectedOutput.appendLine();
-        expectedOutput.appendLine("                 ######    #      #   #######                   ");
-        expectedOutput.appendLine("                 #    ##    #    #    #                         ");
-        expectedOutput.appendLine("                 #    ##     #  #     #                         ");
-        expectedOutput.appendLine("                 ######       ##      #######                   ");
-        expectedOutput.appendLine("                 #    ##      ##      #                         ");
-        expectedOutput.appendLine("                 #    ##      ##      #                         ");
-        expectedOutput.appendLine("                 ######       ##      #######                   ");
-        expectedOutput.appendLine();
-
-        assertEquals(expectedOutput.toString(), outContent.toString());
-    }
-
-    @Test
-    public void testPrintOptions(){
-        inputsBuilder.append("0").append(System.lineSeparator());
-        inputsBuilder.append("11").append(System.lineSeparator());
-
-        preparingInputStream();
-        Main.main(new String[]{});
-
-        expectedOutput.appendLine();
-        expectedOutput.appendLine("****************** Hello! Welcome to SOFTENG 306 Project 2! ******************");
-        expectedOutput.appendLine("Please note this application is not developed in The University of Auckland. All rights reserved for the original developers.");
-        expectedOutput.appendLine("Permission has been granted by the original developers to anonymize the code and use for education purposes.");
-        expectedOutput.appendLine("******************************************************************************************************************************");
-        expectedOutput.appendLine();
-
-        expectedOutput.appendLine("************ I can help you with these functions: *************");
-        expectedOutput.appendLine(" 0. Print Options");
-        expectedOutput.appendLine(" 1. Add a student");
-        expectedOutput.appendLine(" 2. Add a course");
-        expectedOutput.appendLine(" 3. Register student for a course including tutorial/lab classes");
-        expectedOutput.appendLine(" 4. Check available slots in a class (vacancy in a class)");
-        expectedOutput.appendLine(" 5. Print student list by lecture, tutorial or laboratory session for a course");
-        expectedOutput.appendLine(" 6. Enter course assessment components weightage");
-        expectedOutput.appendLine(" 7. Enter coursework mark – inclusive of its components");
-        expectedOutput.appendLine(" 8. Enter exam mark");
-        expectedOutput.appendLine(" 9. Print course statistics");
-        expectedOutput.appendLine("10. Print student transcript");
-        expectedOutput.appendLine("11. Quit Main System");
-        expectedOutput.appendLine();
-
-        expectedOutput.appendLine("Enter your choice, let me help you:");
 
         expectedOutput.appendLine("************ I can help you with these functions: *************");
         expectedOutput.appendLine(" 0. Print Options");
