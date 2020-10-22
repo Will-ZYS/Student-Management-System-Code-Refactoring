9084752f38003cefb10bd31b90e303b4070ba643
Lucas Gao <lucas2005gao@gmail.com>
2020-10-22 13:52:37 +1300

    Changing the student manager scanner to the main scanner

--- src/main/java/com/softeng306/Managers/StudentMgr.java
+++ src/main/java/com/softeng306/Managers/StudentMgr.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
  */
 
 public class StudentMgr {
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = Main.scanner;
 
 
     /**
