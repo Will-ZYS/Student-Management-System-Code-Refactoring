9735c3a299b3497c498ab7c931be4a90fd78fff2
Steven Ho <kuanchenho@gmail.com>
2020-10-27 15:32:35 +1300

    Removed unused method

--- src/main/java/com/softeng306/Interfaces/Database/IProfessorFileMgr.java
+++ src/main/java/com/softeng306/Interfaces/Database/IProfessorFileMgr.java
@@ -8,13 +8,6 @@ import java.util.ArrayList;
 public interface IProfessorFileMgr {
 
     /**
-     * Writes a new professor information into the file.
-     *
-     * @param professor professor to be added into file
-     */
-    void writeProfIntoFile(IProfessor professor);
-
-    /**
      * Load all the professors' information from file into the system.
      *
      * @return an array list of all the professors.
