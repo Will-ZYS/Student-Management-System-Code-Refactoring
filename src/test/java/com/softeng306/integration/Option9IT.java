d0dcd57a245d981cbed3ff366224b1f8035c4645
Lucas Gao <lucas2005gao@gmail.com>
2020-10-28 01:01:44 +1300

    Fixed bug - new line character being different
    Co-authored-by: Martin <MartinTiangco@gmail.com>
    Co-authored-by: Will <yzhb120@aucklanduni.ac.nz>

--- src/test/java/com/softeng306/integration/Option9IT.java
+++ src/test/java/com/softeng306/integration/Option9IT.java
@@ -4,6 +4,9 @@ import com.softeng306.Main;
 import com.softeng306.helper.CommonPrintingHelper;
 import org.junit.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class Option9IT extends IntegrationTestBase{
@@ -30,22 +33,29 @@ public class Option9IT extends IntegrationTestBase{
         expectedOutput.appendLine("Course AU: 3");
         expectedOutput.appendLine();
         expectedOutput.appendLine("Total Slots: 50\tEnrolled Student: 1");
-        expectedOutput.appendLine("Enrollment Rate: 2.00 %");
-        expectedOutput.appendLine();
+        expectedOutput.appendLine(String.format("Enrollment Rate: %4.2f %%\n", (double)2.00));
+
         expectedOutput.appendLine("Main Component: Coursework\tWeight: 40%\t Average: 93.0");
         expectedOutput.appendLine("Sub Component: Labwork\tWeight: 70% (in main component)\t Average: 90.0");
         expectedOutput.appendLine("Sub Component: Presentation\tWeight: 30% (in main component)\t Average: 100.0");
         expectedOutput.appendLine();
         expectedOutput.appendLine("Final Exam\tWeight: 60%\t Average: 88.0");
         expectedOutput.appendLine();
-        expectedOutput.appendLine("Overall Performance: 90.00 ");
-        expectedOutput.appendLine();
+        expectedOutput.appendLine(String.format("Overall Performance: %4.2f \n", (double)90.00));
         expectedOutput.appendLine("***********************************************");
         expectedOutput.appendLine();
 
         CommonPrintingHelper.appendMenu(expectedOutput);
         CommonPrintingHelper.appendByeMessage(expectedOutput);
         //======================================================================
+
+//        byte[] ascii = expectedOutput.toString().getBytes(StandardCharsets.US_ASCII);
+//        String asciiString = Arrays.toString(ascii);
+//        System.out.println(asciiString);
+//
+//        byte[] ascii1 = outContent.toString().getBytes(StandardCharsets.US_ASCII);
+//        String asciiString1 = Arrays.toString(ascii1);
+//        System.out.println(asciiString1);
 
         assertEquals(expectedOutput.toString(), outContent.toString());
     }
