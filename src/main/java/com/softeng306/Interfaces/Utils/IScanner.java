c59dff55872a8e773d72b5ac311259aa76b0e833
Will Zhang <yzhb120@aucklanduni.ac.nz>
2020-10-29 14:17:58 +1300

    Renamed IScannerSingleton to IScanner
    Co-Authored-By: MartinTiangco <50017873+MartinTiangco@users.noreply.github.com>
    Co-Authored-By: Steven Ho <kuanchenho@gmail.com>
    Co-Authored-By: Raymond Hon Sum Chiu <49673907+raymondhonsumchiu@users.noreply.github.com>
    Co-Authored-By: Justin Teo <justinteo97@gmail.com>

--- src/main/java/com/softeng306/Interfaces/Utils/IScanner.java
+++ src/main/java/com/softeng306/Interfaces/Utils/IScanner.java
@@ -1,8 +1,7 @@
 package com.softeng306.Interfaces.Utils;
 
-public interface IScannerSingleton {
+public interface IScanner {
 
-    //TODO not sure if method should be static
     /**
      * Made to reset the Scanner. Main purpose is to ensure consistent testing results
      */
